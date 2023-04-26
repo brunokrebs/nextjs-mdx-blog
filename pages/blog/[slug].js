cd76ff548ea43cb871fb8e5646ab7d2d184def3c
8xu <mischudrag@gmail.com>
2023-04-27 01:08:08 +0300

    Initial commit?

---
+++ pages/blog/[slug].js
@@ -0,0 +1,56 @@
+import { MDXRemote } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
+import matter from 'gray-matter'
+import fs from 'fs'
+import path from 'path'
+
+const PostPage = ({ frontMatter: { title, date }, mdxSource }) => {
+    return (
+        <>
+            <a href='/'>Back</a>
+            <div className='post'>
+                <div className="data">
+                    <h1 className='title'>{title}</h1>
+                    <p className='date'>{date}</p>
+                </div>
+                <div className="content">
+                    <MDXRemote {...mdxSource} />
+                </div>
+            </div>
+        </>
+    )
+  }
+  
+  const getStaticPaths = async () => {
+    const files = fs.readdirSync(path.join('posts'))
+  
+    const paths = files.map(filename => ({
+      params: {
+        slug: filename.replace('.mdx', '')
+      }
+    }))
+  
+    return {
+      paths,
+      fallback: false
+    }
+  }
+  
+    const getStaticProps = async ({ params: { slug } }) => {
+    const markdownWithMeta = fs.readFileSync(path.join('posts',
+      slug + '.mdx'), 'utf-8')
+  
+    const { data: frontMatter, content } = matter(markdownWithMeta)
+    const mdxSource = await serialize(content)
+  
+    return {
+      props: {
+        frontMatter,
+        slug,
+        mdxSource
+      }
+    }
+  }
+  
+  export { getStaticProps, getStaticPaths }
+  export default PostPage
